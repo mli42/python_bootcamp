54105ee4ff0afbc02df1be947766cf468e1dcfde
mli42 <mli@student.42.fr>
2020-11-19 22:31:44 +0100

    init d01/ex05

---
+++ day01/ex05/the_bank.py
@@ -0,0 +1,49 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    the_bank.py                                        :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: mli <mli@student.42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/11/19 22:08:47 by mli               #+#    #+#              #
+#    Updated: 2020/11/19 22:30:59 by mli              ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+class Account(object):
+
+    ID_COUNT = 1
+
+    def __init__(self, name, **kwargs):
+        self.id = self.ID_COUNT
+        self.name = name
+        self.__dict__.update(kwargs)
+        if hasattr(self, 'value'):
+            self.value = 0
+        Account.ID_COUNT += 1
+
+    def transfer(self, amount):
+        self.value += amount
+
+class Bank(object):
+    """The bank"""
+    def __init__(self):
+        self.account = []
+
+    def add(self, account):
+        self.account.append(account)
+
+    def transfer(self, origin, dest, amount):
+        """
+            @origin: int(id) or str(name) of the first account
+            @dest:    int(id) or str(name) of the destination account
+            @amount: float(amount) amount to transfer
+            @return         True if success, False if an error occured
+		"""
+
+    def fix_account(self, account):
+        """
+            fix the corrupted account
+            @account: int(id) or str(name) of the account
+            @return         True if success, False if an error occured
+		"""
