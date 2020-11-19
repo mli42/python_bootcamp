bb5a04ed5e22cbeac85030abde7ff08d0299bfb1
mli42 <mli@student.42.fr>
2020-11-19 22:30:29 +0100

    d01/ex03++

--- day01/ex03/generator.py
+++ day01/ex03/generator.py
@@ -6,56 +6,53 @@
 #    By: mli <mli@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/01/16 23:13:39 by mli               #+#    #+#              #
-#    Updated: 2020/01/17 00:30:11 by mli              ###   ########.fr        #
+#    Updated: 2020/11/19 19:05:50 by mli              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-def my_randfct(words):
-    width = len(words)
-    my_rand = int(width / 1.3)
-    my_rand = (width - my_rand if (width - my_rand > 0 and width - my_rand < width) else -1)
-    if (width == 0):
-        return ('w')
-    if (width % 3 == 0):
-        return (words[-my_rand - int(my_rand / 1.3)])
-    if (width % 2 == 0):
-        return (words[-int(my_rand / 1.6)])
-    return (words[my_rand])
+import random
 
 def generator(text, sep=" ", option=None):
     try:
-        test = 'text'
-        text = str(text)
-        test = 'separator'
-        sep = str(sep)
-        test = 'option'
-        if (option != 'shuffle' and option != 'unique' and option != 'ordered' and option != None):
-            int("Not work")
+        if not isinstance(text, str) or not isinstance(sep, str) \
+        or option not in [None, "shuffle", "unique", "ordered"]:
+            raise ValueError
     except ValueError:
-        return("ERROR")
+        yield ("ERROR")
+        return
 
     lst = text.split(sep)
 
     if (option == 'shuffle'):
-        lst.sort(key=my_randfct)
+        lst.sort(key=lambda x: random.random())
     elif (option == 'unique'):
-        lst.reverse()
-        for words in lst:
-            if (lst.count(words) > 1):
-                lst.remove(words)
-        lst.reverse()
+        lst = [word for i,word in enumerate(lst) if lst[:i].count(word) == 0]
     elif (option == 'ordered'):
         lst.sort()
 
     for words in lst:
         yield (words)
 
-
-#for words in generator("Helloooo how are you ? Im lokking  if it ! works well or not", option='shuffle'):
-#    print("- '%s'" %words)
-
-#for words in generator("Helloooo how are you ?  a if it ! works well or not or a if", option='unique'):
-#    print("- '%s'" %words)
-
-#for words in generator("Helloooo how are you ?  a if it ! works well or not or a if", option='ordered'):
-#    print("- '%s'" %words)
+if __name__ == "__main__":
+    text = "Le Lorem Ipsum est simplement du faux texte."
+    for words in generator(text):
+        print("- '%s'" %words)
+    print("---------------------------------------------------------------")
+    for words in generator(text, option='shuffle'):
+        print("- '%s'" %words)
+    print("---------------------------------------------------------------")
+    for words in generator("3 lorem 3.4 lorem 4 ipsum 4.5 ipsum 5", option='unique'):
+        print("- '%s'" %words)
+    print("---------------------------------------------------------------")
+    for words in generator(text, option='ordered'):
+        print("- '%s'" %words)
+    print("---------------------------------------------------------------")
+    for words in generator(3.4, option='ordered'):
+        print("- '%s'" %words)
+    print("---------------------------------------------------------------")
+    for words in generator("Lorem ipsum", sep=3.4, option='ordered'):
+        print("- '%s'" %words)
+    print("---------------------------------------------------------------")
+    for words in generator("Lorem ipsum", option='Lorem ipsum too'):
+        print("- '%s'" %words)
+    print("---------------------------------------------------------------")
