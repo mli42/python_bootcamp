80fd745ed8daeb482fbd18e3f0a1330f5a930743
mli42 <mli@student.42.fr>
2020-11-21 00:10:28 +0100

    d00/ex100 ETA

--- day00/ex10/loading.py
+++ day00/ex10/loading.py
@@ -6,7 +6,7 @@
 #    By: mli <mli@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/11/20 22:12:23 by mli               #+#    #+#              #
-#    Updated: 2020/11/20 23:27:05 by mli              ###   ########.fr        #
+#    Updated: 2020/11/21 00:09:10 by mli              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,16 +16,22 @@ def ft_progress(lst):
     BARLEN = 23
     LSTLEN = len(lst)
     STARTTIME = time.time()
-    eta = 3.4
+    eta = 0
+    delta = None
+    refresh_rate = LSTLEN / 20
     for i, elem in enumerate(lst, 1):
         ratio = i / LSTLEN
         percentage = ratio * 100
         bar = ("=" * int(ratio * (BARLEN - 1))) + ">"
         current_time = time.time()
         elapsed = current_time - STARTTIME
+        if delta is not None:
+            eta = delta * (LSTLEN - i)
         print("\rETA: %.2fs [%3d%%][%-*s] %d/%d | elapsed time %.2fs"
               %(eta, percentage, BARLEN, bar, i, LSTLEN, elapsed), end="")
         yield (elem)
+        if delta is None or (i % refresh_rate) == 0:
+            delta = time.time() - current_time
 
 if __name__ == "__main__":
 
