7207df584a4d74f388e37d25a58bf4dd1e917ecc
mli42 <mli@student.42.fr>
2020-11-23 00:38:23 +0100

    d03/ex01++

--- day03/ex01/ImageProcessor.py
+++ day03/ex01/ImageProcessor.py
@@ -6,28 +6,30 @@
 #    By: mli <mli@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/01/19 21:40:16 by mli               #+#    #+#              #
-#    Updated: 2020/01/19 23:35:39 by mli              ###   ########.fr        #
+#    Updated: 2020/11/23 00:25:03 by mli              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import numpy as np
 from PIL import Image
 
+# May need: pip install Pillow
+
 class ImageProcessor:
 
     @staticmethod
     def load(path):
-        img = Image.open(path)
-        print("The dimensions of the image are %d x %d." %(img.size[0], img.size[1]))
-        return (np.asarray(img))
+        with Image.open(path) as img:
+            print("The dimensions of the image are %d x %d." %(img.size[0], img.size[1]))
+            ret = np.asarray(img)
+        return (ret)
 
     @staticmethod
     def display(array):
         img = Image.fromarray(array)
         img.show()
 
-'''
-test = ImageProcessor()
-img_array = test.load("../42AI.png")
-test.display(img_array)
-'''
+if __name__ == "__main__":
+    test = ImageProcessor()
+    img_array = test.load("../resources/42AI.png")
+    test.display(img_array)
