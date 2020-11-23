2e2a13088759dfcd16ba17d31aeb98e470a1a5e4
mli42 <mli@student.42.fr>
2020-11-23 22:02:03 +0100

    d03/ex02 ScrapBooker.crop()

--- day03/ex02/ScrapBooker.py
+++ day03/ex02/ScrapBooker.py
@@ -6,32 +6,35 @@
 #    By: mli <mli@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/01/19 23:44:36 by mli               #+#    #+#              #
-#    Updated: 2020/01/20 00:19:35 by mli              ###   ########.fr        #
+#    Updated: 2020/11/23 22:00:09 by mli              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import numpy as np
-from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpltimg
 
 class ScrapBooker:
 
     @staticmethod
     def crop(array, dimensions, position=(0, 0)):
-        img = Image.fromarray(array)
-        if (img.size[0] >= dimensions[0] + position[0] and img.size[1] >= dimensions[1] + position[1]):
-            cropped = img.crop((position[1], position[0], dimensions[1], dimensions[0]))
-            return (np.asarray(cropped))
+        img = array
+        border_max = (position[0] + dimensions[0], position[1] + dimensions[1])
+        if (img.shape[0] >= border_max[0] and img.shape[1] >= border_max[1]):
+            cropped = img[position[0]:border_max[0], position[1]:border_max[1]]
+            return (cropped)
+        # If out of bounds, returns original img?
         return (array)
 
+def showimg(array):
+    plt.imshow(array)
+    plt.show()
 
-'''
-#Load Image
-test_nparray = np.asarray(Image.open("../42AI.png"))
-#img = Image.fromarray(test_nparray)
-#img.show()
+if __name__ == "__main__":
+    scrap = ScrapBooker()
+    path = "../resources/42AI.png"
+    img42ai = mpltimg.imread(path)
+    #showimg(img42ai)
 
-scrap = ScrapBooker()
-# Crop Test
-Image.fromarray(scrap.crop(test_nparray, (30, 100))).show()
-#
-'''
+    # Crop Test
+    showimg(scrap.crop(img42ai, (30, 200), (140, 0)))
