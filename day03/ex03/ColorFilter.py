552add10016283753e1e92119cdbec301d864f24
mli42 <mli@student.42.fr>
2020-11-25 22:59:03 +0100

    d03/ex03 ColorFilter.to_color()

--- day03/ex03/ColorFilter.py
+++ day03/ex03/ColorFilter.py
@@ -6,20 +6,38 @@
 #    By: mli <mli@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/11/24 22:42:30 by mli               #+#    #+#              #
-#    Updated: 2020/11/24 23:34:25 by mli              ###   ########.fr        #
+#    Updated: 2020/11/25 22:57:33 by mli              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpltim
+#import matplotlib.pyplot as plt
+#import matplotlib.image as mpltim
 from ImageProcessor import ImageProcessor
 
 class ColorFilter:
 
 	@staticmethod
 	def invert(array: np.ndarray) -> np.ndarray:
-		return array
+		return np.asarray([1., 1., 1.]) - array[..., :3]
+
+	@staticmethod
+	def to_blue(array: np.ndarray) -> np.ndarray:
+		res = np.zeros(array.shape)
+		res[..., 2] = array[..., 2]
+		res[..., 3] = array[..., 3]
+		return res
+
+	@staticmethod
+	def to_green(array: np.ndarray) -> np.ndarray:
+		res = [0, 1, 0, 1] * array
+		return res
+
+	@staticmethod
+	def to_red(array: np.ndarray) -> np.ndarray:
+		self = ColorFilter()
+		res = array[..., :3] - (self.to_blue(array) + self.to_green(array))[..., :3]
+		return res
 
 if __name__ == "__main__":
 	imgProc = ImageProcessor()
@@ -27,4 +45,8 @@ if __name__ == "__main__":
 	elon = imgProc.load("../resources/elon.png")
 	#imgProc.display(elon)
 
-	imgProc.display(cfilter.invert(elon))
+	#imgProc.display(cfilter.invert(elon))
+	#imgProc.display(cfilter.to_blue(elon))
+	#imgProc.display(cfilter.to_green(elon))
+	#imgProc.display(cfilter.to_red(elon))
+	#imgProc.display(elon)
