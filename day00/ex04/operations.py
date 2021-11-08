fe33035519349b308f510a659f94ae15ce7806c1
mli42 <mli@student.42.fr>
2021-11-08 22:59:03 +0100

    Add editorconfig + update d00/ex04

--- day00/ex04/operations.py
+++ day00/ex04/operations.py
@@ -6,33 +6,36 @@
 #    By: mli <mli@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/01/13 23:28:45 by mli               #+#    #+#              #
-#    Updated: 2020/01/14 00:04:07 by mli              ###   ########.fr        #
+#    Updated: 2021/11/08 22:53:54 by mli              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
 
-def UseError(str=None):
-    if (str != None):
-        print("InputError: " + str + "\n")
-    print("Usage: python operations.py")
-    print("Example:\n\tpython operations.py 10 3")
-    exit()
+argc = len(sys.argv)
 
-sys.argv.pop(0)
-if (len(sys.argv) != 2):
-    UseError("Wrong number of arguments")
+def UseError(string: str = None) -> None:
+    if (string != None):
+        print(f"InputError: {string}\n")
+    print(f"Usage: python {sys.argv[0]} <number1> <number2>")
+    print(f"Example:\n\tpython {sys.argv[0]} 10 3")
+    exit(1)
+
+if (argc < 3):
+    UseError()
+elif (argc > 3):
+    UseError("too many arguments")
 
 try:
-    a = int(sys.argv[0])
-    b = int(sys.argv[1])
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
 except ValueError:
-    UseError("Only numbers in input")
+    UseError("only numbers")
 
 print("Sum:\t\t%d" %(a + b))
 print("Difference:\t%d" %(a - b))
 print("Product:\t%d" %(a * b))
 print("Quotient:\t", end="")
-print("%f" %(a / b)) if (b != 0) else print("ERROR (div by zero)")
+print(f"{a / b}" if (b != 0) else "ERROR (div by zero)")
 print("Remainder:\t", end="")
-print("%d" %(a % b)) if (b != 0) else print("ERROR (mod by zero)")
+print(f"{a % b}" if (b != 0) else "ERROR (mod by zero)")
