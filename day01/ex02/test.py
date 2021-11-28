da0fc719b165bcb6b3ec0a931f03069d3a391295
mli42 <mli@student.42.fr>
2021-11-28 22:34:37 +0100

    d01/ex02 Add sub/mul/div/dot/T + more tests

--- day01/ex02/test.py
+++ day01/ex02/test.py
@@ -6,20 +6,28 @@
 #    By: mli <mli@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/28 15:05:08 by mli               #+#    #+#              #
-#    Updated: 2021/11/28 17:08:58 by mli              ###   ########.fr        #
+#    Updated: 2021/11/28 22:30:14 by mli              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from vector import Vector
 
 def print_sep():
-    print("---------------------------------------------------------------")
+    print("-" * 65)
 
+def add_sep(func):
+    def inner(*args, **kwargs):
+        ret = func(*args, **kwargs)
+        print_sep()
+        return ret
+    return inner
+
+@add_sep
 def print_test(has, expected: str) -> None:
     txt = repr(has) if isinstance(has, Vector) else str(has)
     print(txt, f"\nExpect: {expected}")
-    print_sep()
 
+@add_sep
 def test_one():
     print("# Column vector of shape n * 1")
     v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
@@ -27,7 +35,7 @@ def test_one():
 
     print("# Row vector of shape 1 * n")
     v1 = Vector([0.0, 1.0, 2.0, 3.0])
-    v2 = v1 * 5
+    v2 = 5 * v1
     print_test(v2, "Vector([0.0, 5.0, 10.0, 15.0])")
     print_test(v1 / 2.0, "Vector([[0.0], [0.5], [1.0], [1.5]])")
 
@@ -39,6 +47,7 @@ def test_one():
         print("Expect: ValueError('A scalar cannot be divided by a Vector.')")
         print_sep()
 
+@add_sep
 def test_two():
     print("# Column vector of shape n * 1")
     v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
@@ -50,16 +59,47 @@ def test_two():
     print_test(v2.shape, "(1, 4)")
     print_test(v2.values, "[0.0, 1.0, 2.0, 3.0]")
 
+@add_sep
 def test_three():
     print_test(Vector([0.0, 1.0, 2.0, 3.0]), "[0.0, 1.0, 2.0, 3.0]")
     print_test(Vector([[0.0], [1.0], [2.0], [3.0]]), "[[0.0], [1.0], [2.0], [3.0]]")
     print_test(Vector(3), "[[0.0], [1.0], [2.0]]")
     print_test(Vector(range(10, 15)), "[[10.0], [11.0], [12.0], [13.0], [14.0]]")
 
+@add_sep
+def test_four():
+    v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+    v2 = Vector([0.0, 1.0, 2.0, 3.0])
+
+    print_test(v1.shape, "(4, 1)")
+    print_test(v1.T(), "Vector([0.0, 1.0, 2.0, 3.0])")
+    print_test(v1.T().shape, "(1, 4)")
+
+    print_test(v2.shape, "(1, 4)")
+    print_test(v2.T(), "Vector([[0.0], [1.0], [2.0], [3.0]])")
+    print_test(v2.T().shape, "(4, 1)")
+
+@add_sep
+def test_five():
+    v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+    v2 = Vector([0.0, 1.0, 2.0, 3.0])
+    v3 = Vector([3.0, 2.0, 5.0, 1.0])
+
+    print(v1.dot(v1))
+    print(v2.dot(v2))
+    print(v1.dot(v2))
+    print(v2.dot(v1))
+    print_test(v1 + v1, "[[0.0], [2.0], [4.0], [6.0]]")
+    print_test(v1 - v1, "[[0.0], [0.0], [0.0], [0.0]]")
+    print_test(v2 + v2, "[0.0, 2.0, 4.0, 6.0]")
+    print_test(v2 - v2, "[0.0, 0.0, 0.0, 0.0]")
+
+    print_test(v2 - v3, "[-3.0, -1.0, -3.0, 2.0]")
+    print_test(v3 - v2, "[3.0, 1.0, 3.0, -2.0]")
+
 if __name__ == "__main__":
-    # test_one()
-    print_sep()
+    test_one()
     test_two()
-    print_sep()
     test_three()
-    print_sep()
+    test_four()
+    test_five()
--- day01/ex02/vector.py
+++ day01/ex02/vector.py
@@ -6,7 +6,7 @@
 #    By: mli <mli@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/01/15 17:45:09 by mli               #+#    #+#              #
-#    Updated: 2021/11/28 17:59:04 by mli              ###   ########.fr        #
+#    Updated: 2021/11/28 22:16:34 by mli              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -53,21 +53,21 @@ class Vector:
         return (f"Values: {self.values} | Shape: {self.shape}")
 
     def __str__(self) -> str:
-        return (str(self.values))
+        return (f"Vector({self.values})")
 
-    def get_1x1_value(self) -> int:
-        if self.shape != (1, 1):
-            return 0
-        if isinstance(self.values[0], float):
-            return self.values[0]
-        return self.values[0][0]
+    def get_value(self, index: int) -> float or int:
+        if self.shape[0] < index and self.shape[1] < index:
+            raise ValueError("index out of bound")
+        if isinstance(self.values[index], float):
+            return self.values[index]
+        return self.values[index][0]
 
     def __add__(self, other):
         if not (isinstance(other, Vector) and (other.shape == self.shape)):
             raise ValueError("Addition only between vectors of same shape")
         res = []
         if self.shape == (1, 1):
-            res.append(self.get_1x1_value() + other.get_1x1_value())
+            res.append(self.get_value(0) + other.get_value(0))
         elif self.shape[0] > 1:
             for a, b in zip(self.values, other.values):
                 res.append([a[0] + b[0]])
@@ -78,6 +78,78 @@ class Vector:
 
     def __radd__(self, other):
         return self.__add__(other)
+
+    def __sub__(self, other):
+        if not (isinstance(other, Vector) and (other.shape == self.shape)):
+            raise ValueError("Subtraction only between vectors of same shape")
+        res = []
+        if self.shape == (1, 1):
+            res.append(self.get_value(0) - other.get_value(0))
+        elif self.shape[0] > 1:
+            for a, b in zip(self.values, other.values):
+                res.append([a[0] - b[0]])
+        else: # self.shape[1] > 1
+            for a, b in zip(self.values, other.values):
+                res.append(a - b)
+        return (res)
+
+    def __rsub__(self, other):
+        return self.__sub__(other)
+
+    def __truediv__(self, other):
+        if not isinstance(other, (float, int)):
+            raise ValueError("Truediv only with float/int")
+        if float(other) == 0.0:
+            raise ValueError("Cannot div by 0")
+        res = []
+        if self.shape == (1, 1):
+            res.append(self.get_value(0) / other)
+        elif self.shape[0] > 1:
+            for a in self.values:
+                res.append([a[0] / other])
+        else: # self.shape[1] > 1
+            for a in self.values:
+                res.append(a / other)
+        return (res)
+
+    def __rtruediv__(self, other):
+        raise ValueError('A scalar cannot be divided by a Vector.')
+
+    def __mul__(self, other):
+        if not isinstance(other, (float, int)):
+            raise ValueError("Multiplication only with float/int")
+        res = []
+        if self.shape == (1, 1):
+            res.append(self.get_value(0) * other)
+        elif self.shape[0] > 1:
+            for a in self.values:
+                res.append([a[0] * other])
+        else: # self.shape[1] > 1
+            for a in self.values:
+                res.append(a * other)
+        return (res)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def dot(self, other) -> float or int:
+        if not (isinstance(other, Vector) and \
+            (self.shape == other.shape or self.shape == other.shape[::-1])):
+            raise ValueError("Vector.dot take a vector of same dimension")
+        res = 0
+        length = len(self.values)
+        for i in range(length):
+            res += self.get_value(i) * other.get_value(i)
+        return res
+
+    def T(self):
+        res = []
+        for x in self.values:
+            if isinstance(x, float):
+                res.append([x])
+            else:
+                res.append(x[0])
+        return Vector(res)
 
 '''
 Following are differences (repr vs str):
