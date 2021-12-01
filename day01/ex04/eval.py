0feeaace441188db0661750d63156bd31c9c0561
mli <mli@student.42.fr>
2021-12-01 23:30:24 +0100

    d01/ex04 fix + tests

--- day01/ex04/eval.py
+++ day01/ex04/eval.py
@@ -6,40 +6,48 @@
 #    By: mli <mli@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/01/17 10:08:17 by mli               #+#    #+#              #
-#    Updated: 2020/01/17 10:41:30 by mli              ###   ########.fr        #
+#    Updated: 2021/12/01 23:26:14 by mli              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+
+def evaluate_guard(coefs, words) -> bool:
+    if not all([isinstance(obj, list) for obj in [coefs, words]]):
+        return False
+    if len(coefs) != len(words):
+        return False
+    if not all([isinstance(obj, str) for obj in words]):
+        return False
+    if not all([isinstance(obj, (int, float)) for obj in coefs]):
+        return False
+    return True
 
 class Evaluator:
 
     @staticmethod
     def zip_evaluate(coefs, words):
-        if (len(coefs) != len(words)):
+        if not evaluate_guard(coefs, words):
             return (-1)
         res = 0
-        try:
-            for word, nb in zip(words, coefs):
-                res += len(str(word)) * float(nb)
-        except ValueError:
-            return (-1)
+        for word, nb in zip(words, coefs):
+            res += len(word) * nb
         return (res)
 
+    @staticmethod
     def enumerate_evaluate(coefs, words):
-        if (len(coefs) != len(words)):
+        if not evaluate_guard(coefs, words):
             return (-1)
         res = 0
-        try:
-            for i, word in enumerate(words):
-                res += len(str(word)) * float(coefs[i])
-        except ValueError:
-            return (-1)
+        for i, word in enumerate(words):
+            res += len(word) * coefs[i]
         return (res)
 
-#words = ["Le", "Lorem", "Ipsum", "est", "simple"]
-#coefs = [1.0, 2.0, 1.0, 4.0, 0.5]
+if __name__ == "__main__":
+    def test_evaluate(coefs, words):
+        print(f"Test for {coefs}/{words}:")
+        print(Evaluator.enumerate_evaluate(coefs, words))
+        print(Evaluator.zip_evaluate(coefs, words))
+        print()
 
-#words = ["Le", "Lorem", "Ipsum", "n'", "est", "pas", "simple"]
-#coefs = [0.0, -1.0, 1.0, -12.0, 0.0, 42.42]
-
-#print(Evaluator.enumerate_evaluate(coefs, words))
-#print(Evaluator.zip_evaluate(coefs, words))
+    test_evaluate([1.0, 2.0, 1.0, 4.0, 0.5], ["Le", "Lorem", "Ipsum", "est", "simple"])
+    test_evaluate([0.0, -1.0, 1.0, -12.0, 0.0, 42.42], ["Le", "Lorem", "Ipsum", "n'", "est", "pas", "simple"])
+    test_evaluate([1, 2, 3], ["word", 2, "wordo"])
