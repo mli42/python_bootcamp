5f840c0ab9f5411ac367533c5ffdb90dc06b0dbf
mli <mli@student.42.fr>
2021-12-01 22:47:57 +0100

    d01/ex03 no try/catch

--- day01/ex03/generator.py
+++ day01/ex03/generator.py
@@ -6,25 +6,22 @@
 #    By: mli <mli@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/01/16 23:13:39 by mli               #+#    #+#              #
-#    Updated: 2020/11/19 19:05:50 by mli              ###   ########.fr        #
+#    Updated: 2021/12/01 22:33:55 by mli              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-import random
+from random import random
 
 def generator(text, sep=" ", option=None):
-    try:
-        if not isinstance(text, str) or not isinstance(sep, str) \
+    if not isinstance(text, str) or not isinstance(sep, str) \
         or option not in [None, "shuffle", "unique", "ordered"]:
-            raise ValueError
-    except ValueError:
         yield ("ERROR")
         return
 
     lst = text.split(sep)
 
     if (option == 'shuffle'):
-        lst.sort(key=lambda x: random.random())
+        lst.sort(key=lambda x: random())
     elif (option == 'unique'):
         lst = [word for i,word in enumerate(lst) if lst[:i].count(word) == 0]
     elif (option == 'ordered'):
@@ -41,10 +38,10 @@ if __name__ == "__main__":
     for words in generator(text, option='shuffle'):
         print("- '%s'" %words)
     print("---------------------------------------------------------------")
-    for words in generator("3 lorem 3.4 lorem 4 ipsum 4.5 ipsum 5", option='unique'):
+    for words in generator(text, option='ordered'):
         print("- '%s'" %words)
     print("---------------------------------------------------------------")
-    for words in generator(text, option='ordered'):
+    for words in generator("3 lorem 3.4 lorem 4 ipsum 4.5 ipsum 5", option='unique'):
         print("- '%s'" %words)
     print("---------------------------------------------------------------")
     for words in generator(3.4, option='ordered'):
