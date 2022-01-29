2384b552f3ff56b67ef3b0ae0a8646f8631fb359
mli42 <mli@student.42.fr>
2022-01-29 16:24:05 +0100

    d03/ex01 Done

--- day03/ex01/ImageProcessor.py
+++ day03/ex01/ImageProcessor.py
@@ -6,30 +6,48 @@
 #    By: mli <mli@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/01/19 21:40:16 by mli               #+#    #+#              #
-#    Updated: 2020/11/23 00:25:03 by mli              ###   ########.fr        #
+#    Updated: 2022/01/29 16:19:35 by mli              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import numpy as np
-from PIL import Image
-
-# May need: pip install Pillow
+import matplotlib.pyplot as plt
+import matplotlib.image as mpltimg
+from sys import stderr
 
 class ImageProcessor:
 
-    @staticmethod
-    def load(path):
-        with Image.open(path) as img:
-            print("The dimensions of the image are %d x %d." %(img.size[0], img.size[1]))
-            ret = np.asarray(img)
-        return (ret)
+    def __init__(self):
+        pass
 
     @staticmethod
-    def display(array):
-        img = Image.fromarray(array)
-        img.show()
+    def load(path: str) -> np.ndarray or None:
+        img = None
+        try:
+            img = mpltimg.imread(path)
+            print("The dimensions of the image are %d x %d." %(img.shape[0], img.shape[1]))
+        except Exception as e:
+            print(f"Exception: {e.__class__.__name__}: {e.strerror if hasattr(e, 'strerror') else e}", file=stderr)
+        return (img)
+
+    @staticmethod
+    def display(array: np.ndarray):
+        plt.axis("off")
+        plt.imshow(array)
+        plt.show()
+
+def main():
+    imgProcessor = ImageProcessor()
+    def test_img(path: str):
+        print(f"Loading path: {path}")
+        img_array = imgProcessor.load(path)
+        print("Loading returned:", img_array)
+        if (img_array is not None):
+            imgProcessor.display(img_array)
+
+    test_img("../resources/42AI.png")
+    test_img("../resources/coucou.png")
+    test_img("../resources/couco.png")
 
 if __name__ == "__main__":
-    test = ImageProcessor()
-    img_array = test.load("../resources/42AI.png")
-    test.display(img_array)
+    main()
