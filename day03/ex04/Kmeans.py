63bcb69e2ccc8ee12db2679120135b3d178fbef0
mli42 <mli@student.42.fr>
2022-03-14 00:24:29 +0100

    d03/ex04 Parsing done

--- day03/ex04/Kmeans.py
+++ day03/ex04/Kmeans.py
@@ -6,17 +6,19 @@
 #    By: mli <mli@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/08 17:04:24 by mli               #+#    #+#              #
-#    Updated: 2020/12/10 22:10:15 by mli              ###   ########.fr        #
+#    Updated: 2022/03/14 00:21:33 by mli              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+import sys
+import re # Regex
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from mpl_toolkits.mplot3d import Axes3D
 
 class KmeansClustering:
-    def __init__(self, max_iter=20, ncentroid=5):
+    def __init__(self, max_iter=20, ncentroid=4):
         self.ncentroid = ncentroid # number of centroids
         self.max_iter = max_iter # number of max iterations to update the centroids
         self.centroids = [] # values of the centroids
@@ -70,8 +72,38 @@ class KmeansClustering:
                     cluster_centers[i, 2], color=colorstr[i], marker="o", s=150, label="centroids")
         plt.show()
 
-if __name__ == "__main__":
-    data = np.genfromtxt("../resources/solar_system_census.csv", delimiter=",", skip_header=1)
+ARGS_NAME = ['filepath', 'ncentroid', 'max_iter']
+
+def parsing() -> list or None:
+    if (len(sys.argv) != 4):
+        return None
+    args_regex = [
+        rf"^{ARGS_NAME[0]}=(.+\.csv)$",
+        rf"^{ARGS_NAME[1]}=(\d+)$",
+        rf"^{ARGS_NAME[2]}=(\d+)$",
+    ]
+    res = []
+
+    for i, regex in enumerate(args_regex):
+        search_obj = re.search(args_regex[i], sys.argv[i + 1])
+        if (search_obj is None):
+            return None
+        res.append(search_obj.group(1))
+    return res
+
+def print_usage():
+    print(f"""USAGE:
+    python {sys.argv[0]} %s=PATH %s=NB %s=NB
+EXAMPLE:
+    python {sys.argv[0]} %s=../resources/solar_system_census.csv %s=4 %s=30
+    """ %(*ARGS_NAME, *ARGS_NAME))
+
+def main():
+    ARGV = parsing()
+    if ARGV is None:
+        print_usage()
+        return
+    data = np.genfromtxt(ARGV[0], delimiter=",", skip_header=1)
     X = data[:, 1:] # Delete index
     ncentroid = 4
 
@@ -79,3 +111,6 @@ if __name__ == "__main__":
     kms.fit(X)
     kms.predict(X)
     kms.fig_3D(X)
+
+if __name__ == "__main__":
+    main()
