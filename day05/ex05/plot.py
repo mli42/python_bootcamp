563d2b0642b9a9802a474c0d14f6a60054b587f0
mli42 <mli@student.42.fr>
2022-08-12 19:21:10 +0200

    d05/ex05: plot

--- day05/ex05/plot.py
+++ day05/ex05/plot.py
@@ -6,7 +6,7 @@
 #    By: mli <mli@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/13 22:28:37 by mli               #+#    #+#              #
-#    Updated: 2020/12/13 22:57:10 by mli              ###   ########.fr        #
+#    Updated: 2022/08/12 19:13:16 by mli              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -24,13 +24,22 @@ def plot(x: np.ndarray, y: np.ndarray, theta: np.ndarray) -> None:
     Raises:
       This function should not raise any Exceptions.
     """
+    if (
+        not all([isinstance(obj, np.ndarray) for obj in [x, y, theta]])
+        or x.size == 0
+        or theta.shape not in [(2,), (2, 1)]
+        or x.shape not in [(x.size,), (x.size, 1)]
+        or x.shape != y.shape
+        ):
+        print('Incorrect parameter')
+        return None
     h = lambda x: theta[0] + theta[1] * x
     plt.plot(x, y, "o")
     plt.plot(x, h(x))
     plt.show()
 
 if __name__ == "__main__":
-    x = np.arange(1,6).reshape(5, 1)
+    x = np.arange(1,6)
     y = np.array([3.74013816, 3.61473236, 4.57655287, 4.66793434, 5.95585554])
 
     flag = 7
