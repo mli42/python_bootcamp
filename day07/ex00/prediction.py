594627452e19e1ee120c82011405fc1bcc77924a
mli42 <mli@student.42.fr>
2022-08-29 14:31:19 +0200

    d07/ex00: prediction.py + unittest

--- day07/ex00/prediction.py
+++ day07/ex00/prediction.py
@@ -6,11 +6,22 @@
 #    By: mli <mli@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/12/22 15:03:43 by mli               #+#    #+#              #
-#    Updated: 2020/12/22 15:34:40 by mli              ###   ########.fr        #
+#    Updated: 2022/08/29 14:24:47 by mli              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import numpy as np
+from typing import Tuple
+
+
+def __check_arrays(arrays: Tuple[np.ndarray]) -> bool:
+    return all([
+        isinstance(obj, np.ndarray)
+        and obj.dtype.kind in 'iuf'
+        and len(obj.shape) == 2
+        and obj.size != 0
+        for obj in arrays])
+
 
 def simple_predict(x: np.ndarray, theta: np.ndarray) -> np.ndarray:
     """Computes the prediction vector y_hat from two non-empty numpy.ndarray.
@@ -24,34 +35,15 @@ def simple_predict(x: np.ndarray, theta: np.ndarray) -> np.ndarray:
     Raises:
       This function should not raise any Exception.
     """
+    if (
+        __check_arrays((x, theta)) is False
+        or theta.size != theta.shape[0]
+        or x.shape[1] + 1 != theta.size
+    ):
+        return None
     m, n = x.shape
-    y_hat = np.zeros((m, 1))
-    y_hat[..., 0] = theta[0, 0]
+    y_hat = np.full(shape=(m, 1), fill_value=theta[0][0])
     for i in range(m):
         for j in range(n):
             y_hat[i][0] += x[i][j] * theta[j + 1][0]
     return y_hat
-
-if __name__ == "__main__":
-    x = np.arange(1, 13).reshape((4, -1))
-
-    # Example 1:
-    theta1 = np.array([5, 0, 0, 0]).reshape(-1, 1)
-    print(simple_predict(x, theta1))
-    # Ouput: array([5., 5., 5., 5.])
-
-    # Example 2:
-    theta2 = np.array([0, 1, 0, 0]).reshape(-1, 1)
-    print(simple_predict(x, theta2))
-    # Output: array([ 1.,  4.,  7., 10.])
-
-    # Example 3:
-    theta3 = np.array([-1.5, 0.6, 2.3, 1.98]).reshape(-1, 1)
-    print(simple_predict(x, theta3))
-    # Output: array([ 9.64, 24.28, 38.92, 53.56])
-
-
-    # Example 4:
-    theta4 = np.array([-3, 1, 2, 3.5]).reshape(-1, 1)
-    print(simple_predict(x, theta4))
-    # Output: array([12.5, 32. , 51.5, 71. ])
---
+++ day07/ex00/test.py
@@ -0,0 +1,27 @@
+from prediction import simple_predict
+import numpy as np
+import unittest
+
+
+def assertEqual(x, y):
+    np.testing.assert_array_almost_equal(x, y)
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_subject(self):
+        x = np.arange(1,13).reshape((4,-1))
+
+        theta1 = np.array([5, 0, 0, 0]).reshape((-1, 1))
+        theta2 = np.array([0, 1, 0, 0]).reshape((-1, 1))
+        theta3 = np.array([-1.5, 0.6, 2.3, 1.98]).reshape((-1, 1))
+        theta4 = np.array([-3, 1, 2, 3.5]).reshape((-1, 1))
+
+        assertEqual(simple_predict(x, theta1), np.array([[5.], [5.], [5.], [5.]]))
+        assertEqual(simple_predict(x, theta2), np.array([[ 1.], [ 4.], [ 7.], [10.]]))
+        assertEqual(simple_predict(x, theta3), np.array([[ 9.64], [24.28], [38.92], [53.56]]))
+        assertEqual(simple_predict(x, theta4), np.array([[12.5], [32. ], [51.5], [71. ]]))
+
+
+if __name__ == '__main__':
+    unittest.main()
